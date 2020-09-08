2bf2eb5ac424c710c93f0f1e71d1be992214178d
spara7c5 <paracchino@hotmail.it>
2020-09-08 17:19:31 +0200

    sostituiti semafori con signal (thread flag)

--- main.c
+++ main.c
@@ -72,10 +72,13 @@
 
 #include "nrf_drv_timer.h"
 #include "nrf_queue.h"
+#include "nrf_drv_pwm.h"
 
 #include "modem.h"
 #include "core/net.h"
 #include "debug.h"
+
+#include "cmsis_os2.h"
 
 #if LEDS_NUMBER <= 2
 #error "Board is not equipped with enough amount of LEDs"
@@ -92,14 +95,68 @@ extern NetInterface *interface;
 
 const nrf_drv_timer_t TIMER_DATA = NRF_DRV_TIMER_INSTANCE(0);
 void timer_dummy_event_handler(nrf_timer_event_t event_type, void* p_context){}
-NRF_QUEUE_DEF(uint32_t, datain_queue, 4, NRF_QUEUE_MODE_OVERFLOW);
-static SemaphoreHandle_t datain_semaph;
-uint32_t datain_buff[8];
+NRF_QUEUE_DEF(uint32_t, datain_queue, 8, NRF_QUEUE_MODE_OVERFLOW); // analog readouts of bit timelengths
+//static SemaphoreHandle_t datain_semaph;
+//static SemaphoreHandle_t butt_semaph;
+//static SemaphoreHandle_t message_semaph;
+#define DATAIN_SIGNAL 0x00000001 // used to save cpu time (avoid polling the queue)
+#define MESS_PROC_SIGNAL 0x00000002 // used as mutex to protect buffer
+
+#define BUTT_SIGNAL 0x00000001 // used to save cpu time (block the thread until the button)
+#define MESS_REC_SIGNAL 0x00000002 // used as mutex to protect buffer
+uint8_t datain_buff;
+#define MESS_REPLY_TIMEOUT 1000 //1 sec
+
+static nrf_drv_pwm_t m_pwm0 = NRF_DRV_PWM_INSTANCE(0);
 
 
 extern void uart_drv_event_handler(nrf_drv_uart_event_t * p_event, void* p_context);
 nrf_drv_uart_t UARTE_inst0=NRF_DRV_UART_INSTANCE(0);
 //nrf_drv_uart_t UARTE_inst1=NRF_DRV_UART_INSTANCE(1);
+
+typedef enum {
+  SUCCESS=0,
+  WRONG_BIT_TIME,
+  WRONG_MEX_PROCESS,
+
+}error_smartbutt_t;
+
+#define BITZEROMIN  4000  
+#define BITZEROMAX  5000
+#define BITONEMIN    6000
+#define BITONEMAX   10000
+
+
+
+#define TASK_DELAY        100           /**< Task delay. Delays a LED0 task for 200 ms */
+#define TIMER_PERIOD      1000          /**< Timer period. LED1 timer will expire after 1000 ms */
+
+TaskHandle_t  modem_task_handle;   /**< Reference to LED0 toggling FreeRTOS task. */  
+TimerHandle_t dataIN_task_handle;  
+TimerHandle_t main_task_handle;  
+UBaseType_t stackwater;
+
+
+
+#define BUZZPRESS vTaskDelay(100); nrf_gpio_pin_set(BUZZ); nrf_delay_ms(400); nrf_gpio_pin_clear(BUZZ) 
+#define BUZZERR  for(uint8_t i=0;i<3;i++){nrf_gpio_pin_set(BUZZ); nrf_delay_ms(50); nrf_gpio_pin_clear(BUZZ);nrf_delay_ms(50);} 
+
+
+
+////////////////////   utils  ///////////////////////
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
+
+//////////////////////////////////////////////////////
 
 
 void uart_error_handle(app_uart_evt_t * p_event)
@@ -113,24 +170,6 @@ void uart_error_handle(app_uart_evt_t * p_event)
         APP_ERROR_HANDLER(p_event->data.error_code);
     }
 }
-
-
-#define TASK_DELAY        100           /**< Task delay. Delays a LED0 task for 200 ms */
-#define TIMER_PERIOD      1000          /**< Timer period. LED1 timer will expire after 1000 ms */
-
-TaskHandle_t  modem_task_handle;   /**< Reference to LED0 toggling FreeRTOS task. */  
-TimerHandle_t dataIN_task_handle;  /**< Reference to LED1 toggling FreeRTOS timer. */
-UBaseType_t stackwater;
-
-
- extern SemaphoreHandle_t dl_send_lockHandle;
- extern SemaphoreHandle_t sl_send_lockHandle;
- extern SemaphoreHandle_t ph_send_lockHandle;
-
-/**@brief LED0 task entry function.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the task.
- */
 
  char mex[]="DETECTED\n";
  char a='a';
@@ -239,39 +278,54 @@ void gpiote_callback(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action){
 //  bsp_board_led_invert(BSP_BOARD_LED_3);
 //  nrf_gpio_pin_write(LED_4,0); //light up the LED with 0
 //  bsp_board_led_on(BSP_BOARD_LED_3);
-  UNUSED_VARIABLE(osSemaphoreRelease(datain_semaph));
-  if(datagotinit==0){
-    datastarttime=nrfx_timer_capture(&TIMER_DATA,NRF_TIMER_CC_CHANNEL0);
-     //nrf_drv_timer_clear(&TIMER_LED);
-    datagotinit=1;
-  }else{
-      datagotinit=0;
-      dataendtime=nrfx_timer_capture(&TIMER_DATA,NRF_TIMER_CC_CHANNEL0);
-    if(dataendtime>datastarttime){
-      dataperiod=(dataendtime-datastarttime)/16;
-      ret_code_t err_code = nrf_queue_push(&datain_queue, &dataperiod);
-      APP_ERROR_CHECK(err_code);
-     // __NOP();
-      }else{
-      __NOP();
-      //TODO: resend data or error or difference?
-      }
-   }
+  
+  if (pin==DATA_IN){
+    osThreadFlagsSet(dataIN_task_handle,DATAIN_SIGNAL);
+    if(datagotinit==0){
+      datastarttime=nrfx_timer_capture(&TIMER_DATA,NRF_TIMER_CC_CHANNEL0);
+       //nrf_drv_timer_clear(&TIMER_LED);
+      datagotinit=1;
+    }else{
+        datagotinit=0;
+        dataendtime=nrfx_timer_capture(&TIMER_DATA,NRF_TIMER_CC_CHANNEL0);
+      if(dataendtime>datastarttime){
+        dataperiod=(dataendtime-datastarttime)/16;
+        ret_code_t err_code = nrf_queue_push(&datain_queue, &dataperiod);
+        APP_ERROR_CHECK(err_code);
+       // __NOP();
+        }else{
+        __NOP();
+        //TODO: resend data or error or difference?
+        }
+     }
 
-}
+  }else if(pin==BUTT){
+    osThreadFlagsSet(main_task_handle,BUTT_SIGNAL);
+      
+
+  }
+
+ }//end gpio_callback
 
 void bsp_ext_init(void){
 
     nrfx_err_t err_code = nrf_drv_gpiote_init();
     APP_ERROR_CHECK(err_code);
 
-    nrf_drv_gpiote_in_config_t in_config = NRFX_GPIOTE_CONFIG_IN_SENSE_LOTOHI(true);
-    in_config.pull = GPIO_PIN_CNF_PULL_Disabled;
+    nrf_gpio_cfg_output(BUZZ);
+    nrf_gpio_pin_clear(BUZZ);
+    
 
+    nrf_drv_gpiote_in_config_t in_config = NRFX_GPIOTE_CONFIG_IN_SENSE_LOTOHI(true);
+    in_config.pull = GPIO_PIN_CNF_PULL_Pulldown;
     err_code = nrf_drv_gpiote_in_init(DATA_IN, &in_config, gpiote_callback);
     APP_ERROR_CHECK(err_code);
-
     nrf_drv_gpiote_in_event_enable(DATA_IN, true);
+
+    nrf_drv_gpiote_in_config_t butt_config = NRFX_GPIOTE_CONFIG_IN_SENSE_HITOLO(true);
+    err_code = nrf_drv_gpiote_in_init(BUTT, &butt_config, gpiote_callback);
+    APP_ERROR_CHECK(err_code);
+    nrf_drv_gpiote_in_event_enable(BUTT, true);
 
     }
 
@@ -356,24 +410,60 @@ static void timer_init(void){
 
 }
 
+
+static error_smartbutt_t process_message(uint8_t* mex){
+
+  return SUCCESS;
+}
+
 static void dataIN_task_function (void * pvParameter){
-  uint32_t dataperiodout=0;
+
+    uint32_t dataperiodout=0;
+    ret_code_t err_code=NRF_SUCCESS;
+    uint8_t bit_cnt=0;
 
     while(1){
-      UNUSED_RETURN_VALUE(xSemaphoreTake(datain_semaph, portMAX_DELAY));
-      uint8_t cnt=0;
-      while (cnt<(8+1)){
-        ret_code_t err_code = nrf_queue_generic_pop(&datain_queue,&dataperiodout,false);
-        datain_buff[cnt]=dataperiodout;
-        if(err_code==NRF_SUCCESS) cnt++;
+      osThreadFlagsWait(DATAIN_SIGNAL,0,osWaitForever);
+      bit_cnt=0;
+      while (bit_cnt<(8+1)){
+         err_code= nrf_queue_generic_pop(&datain_queue,&dataperiodout,false);
+         if(err_code==NRF_SUCCESS){
+            if(BITZEROMIN<dataperiodout && dataperiodout<BITZEROMAX)datain_buff&=~(0b0000001<<bit_cnt);
+            else if (BITONEMIN<dataperiodout && dataperiodout<BITONEMAX)datain_buff|=(0b0000001<<bit_cnt);
+            else break;
+            bit_cnt++;
+         }else break; // error in queue (rare)
       }
-      vTaskDelay(1000);
-      TRACE_INFO("data period:%u-%u-%u-%u-%u-%u-%u-%u\n\r",datain_buff[0],datain_buff[1],datain_buff[2],datain_buff[3],datain_buff[4],datain_buff[5],datain_buff[6],datain_buff[7]);
+
+      if (bit_cnt==8){ //all bits are valid
+          osThreadFlagsSet(main_task_handle,MESS_REC_SIGNAL);
+          osThreadFlagsWait(MESS_PROC_SIGNAL,0,osWaitForever); // stop writing on datain_buff 
+      }// the "else" is managed by the main function which timeouts waiting for vali message
+
+      
+      
     }
 }
 
 
 
+
+
+static void main_task_function (void * pvParameter){
+
+  while(1){
+    //uint32_t flag= osThreadFlagsGet ();
+    osThreadFlagsWait(BUTT_SIGNAL,0,osWaitForever);
+    
+    BUZZPRESS;
+    if(osThreadFlagsWait(MESS_REC_SIGNAL,0,MESS_REPLY_TIMEOUT)==osErrorTimeout) {
+      BUZZERR
+    }
+    
+    TRACE_INFO("message:"BYTE_TO_BINARY_PATTERN"\n",BYTE_TO_BINARY(datain_buff));
+    osThreadFlagsSet(dataIN_task_handle,MESS_PROC_SIGNAL);
+  }
+}
 
 int main(void)
 {
@@ -386,9 +476,6 @@ int main(void)
     err_code = nrf_drv_clock_init();
     APP_ERROR_CHECK(err_code);
 
-    datain_semaph = xSemaphoreCreateBinary();
-    ASSERT(NULL != datain_semaph);
-
     /* Configure LED-pins as outputs */
     bsp_board_init(BSP_INIT_LEDS);
     uart_tool_init();
@@ -399,9 +486,9 @@ int main(void)
 
  
     /* Create task for LED0 blinking with priority set to 2 */
-    UNUSED_VARIABLE(xTaskCreate(modem_task_function, "MODEM", configMINIMAL_STACK_SIZE +500 , NULL, 2, &modem_task_handle));
+    //UNUSED_VARIABLE(xTaskCreate(modem_task_function, "MODEM", configMINIMAL_STACK_SIZE +500 , NULL, 2, &modem_task_handle));
     UNUSED_VARIABLE(xTaskCreate(dataIN_task_function, "DATAIN", configMINIMAL_STACK_SIZE +200 , NULL, 2, &dataIN_task_handle));
-    //  UNUSED_VARIABLE(xTaskCreate(StartSendTask, "MODEMTX", configMINIMAL_STACK_SIZE +500 , NULL, 2, &modemTX_task_handle));
+    UNUSED_VARIABLE(xTaskCreate(main_task_function, "MAIN", configMINIMAL_STACK_SIZE +200 , NULL, 2, &main_task_handle));
 
 
     
@@ -425,3 +512,57 @@ int main(void)
 /**
  *@}
  **/
+
+
+
+//static void demo3(void)
+//{
+//    //NRF_LOG_INFO("Demo 3");
+//
+//    /*
+//     * This demo uses only one channel, which is reflected on LED 1.
+//     * The LED blinks three times (200 ms on, 200 ms off), then it stays off
+//     * for one second.
+//     * This scheme is performed three times before the peripheral is stopped.
+//     */
+//
+//    nrf_drv_pwm_config_t const config0 =
+//    {
+//        .output_pins =
+//        {
+//            BUZZ | NRF_DRV_PWM_PIN_INVERTED, // channel 0
+//            NRF_DRV_PWM_PIN_NOT_USED,             // channel 1
+//            NRF_DRV_PWM_PIN_NOT_USED,             // channel 2
+//            NRF_DRV_PWM_PIN_NOT_USED,             // channel 3
+//        },
+//        .irq_priority = APP_IRQ_PRIORITY_LOWEST,
+//        .base_clock   = NRF_PWM_CLK_125kHz,
+//        .count_mode   = NRF_PWM_MODE_UP,
+//        .top_value    = 25000,
+//        .load_mode    = NRF_PWM_LOAD_COMMON,
+//        .step_mode    = NRF_PWM_STEP_AUTO
+//    };
+//    APP_ERROR_CHECK(nrf_drv_pwm_init(&m_pwm0, &config0, NULL));
+//    m_used |= USED_PWM(0);
+//
+//    // This array cannot be allocated on stack (hence "static") and it must
+//    // be in RAM (hence no "const", though its content is not changed).
+//    static uint16_t /*const*/ seq_values[] =
+//    {
+//        0x8000,
+//             0,
+//        0x8000,
+//             0,
+//        0x8000,
+//             0
+//    };
+//    nrf_pwm_sequence_t const seq =
+//    {
+//        .values.p_common = seq_values,
+//        .length          = NRF_PWM_VALUES_LENGTH(seq_values),
+//        .repeats         = 0,
+//        .end_delay       = 4
+//    };
+//
+//    (void)nrf_drv_pwm_simple_playback(&m_pwm0, &seq, 3, NRF_DRV_PWM_FLAG_STOP);
+//}
